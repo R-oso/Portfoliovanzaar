1e4e80182a3747c8baf52bc0c6d908e3d6632852
R-oso <luukroso@gmail.com>
2023-11-08 11:15:56 +0100

    Rightside image loop changes

--- src/components/Rightside.jsx
+++ src/components/Rightside.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import projectsCSS from "../css/projects.module.css";
+import { Link } from "react-router-dom";
 
-const Rightside = (rightSideData) => {
-  return (
-    <div className={projectsCSS.side}>
-      <div className={projectsCSS.flex_container}>
-        <img src="/project_images/Teaparty_cover.png"></img>
-        <img src="/project_images/Teaparty_extras.png"></img>
+const Rightside = ({ rightSideData }) => {
+  if (rightSideData && rightSideData.length > 0) {
+    return (
+      <div className={projectsCSS.side}>
+        <div className={projectsCSS.flex_container}>
+          {rightSideData.map((project, index) => (
+            <Link key={index} to={`/projects/${project.name}`} state={{ layoutType: project.layoutType }}>
+              <div className={projectsCSS.img_container}>
+                <img className={projectsCSS.img} src={`/project_images/${project.link}`} alt={project.title} />
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return <div>No data available</div>;
 };
 
 export default Rightside;
