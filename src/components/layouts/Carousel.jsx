36fa4329a5ae9b93e424072a9c7f4ce1055e70aa
R-oso <luukroso@gmail.com>
2023-11-11 00:29:38 +0100

    New component added

---
+++ src/components/layouts/Carousel.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import VideoCSS from "./videolayout.module.css";
+
+const Carousel = ({ images }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, [images]);
+
+  return (
+    <div className={VideoCSS.carousel}>
+      <img className={VideoCSS.carouselImage} src={`/project_images/${images[currentImageIndex]}`} alt={`Carousel Image ${currentImageIndex}`} />
+    </div>
+  );
+};
+
+export default Carousel;
