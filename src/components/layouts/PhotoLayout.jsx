4af6f6a66e6a6812fda4fa3bafe5ca9770a32cff
R-oso <luukroso@gmail.com>
2023-11-14 21:41:34 +0100

    First commit for photo layout

--- src/components/layouts/PhotoLayout.jsx
+++ src/components/layouts/PhotoLayout.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-const PhotoLayout = () => {
-  const { id, title, description, layoutType, images, info } = props.project;
-  return <div>{title}</div>;
+import PhotoCSS from "./PhotoLayout.module.css";
+
+const PhotoLayout = (props) => {
+  const { images, videos, links, carousel, cover, name } = props.project;
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  return (
+    <section className={PhotoCSS.section}>
+      <div className={PhotoCSS.wrapper}>
+        <div className={PhotoCSS.columns}>
+          {images.map((image, index) => (
+            <div key={index} className={`${PhotoCSS.col} ${PhotoCSS[`image_${index + 1}`]}`}>
+              <img className={PhotoCSS.image} key={index} src={`/project_images/${image}`} alt={`Image ${index + 1}`} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default PhotoLayout;
