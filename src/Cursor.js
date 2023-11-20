18c09a0d162fbe2b3513615d3663bb0beb8dba2c
R-oso <luukroso@gmail.com>
2023-11-20 13:50:24 +0100

    Commented out cursor code and added about page

--- src/Cursor.js
+++ src/Cursor.js
@@ -1,8 +1,8 @@
-const auraCursor = document.createElement("div");
-auraCursor.classList.add("aura_cursor");
-document.body.appendChild(auraCursor);
+// const auraCursor = document.createElement("div");
+// auraCursor.classList.add("aura_cursor");
+// document.body.appendChild(auraCursor);
 
-document.addEventListener("mousemove", (e) => {
-  auraCursor.style.left = `${e.clientX}px`;
-  auraCursor.style.top = `${e.clientY}px`;
-});
+// document.addEventListener("mousemove", (e) => {
+//   auraCursor.style.left = `${e.clientX}px`;
+//   auraCursor.style.top = `${e.clientY}px`;
+// });
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import AboutCSS from "../css/about.module.css";
 
 const About = () => {
-  return;
+  return (
+    <section className={AboutCSS.section}>
+      <h1 className={AboutCSS.h1}>Yo Yo i'm zara.....</h1>
+      <h2>I AM CREATIVE GUYS!!!!</h2>
+    </section>
+  );
 };
 
 export default About;
